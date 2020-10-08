38940685d577f90185291727d0d764c603abab51
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-10-08 20:09:39 +0900

    3rd

---
+++ front/src/App.vue
@@ -0,0 +1,6 @@
+<div class="imgContent">
+          <div class="imagePreview">  
+          <img :src="uploadedImage" style="width:100%;" />
+             <input type="file" class="file_input" name="photo" @change="onFileChange"  accept="image/*" />
+          </div>
+        </div>
